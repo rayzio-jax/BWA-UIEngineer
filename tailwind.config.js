950da8d5fc63a2d44320d7c5a2ae0890a0bd190b
rayhan <rayhananthaakbar@gmail.com>
2024-10-03 20:34:56 +0700

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			fontFamily: {
+				head: ["Montserrat", "sans-serif"],
+				aside: ["Inter", "sans-serif"],
+				sans: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				"btn-primary": "#7c3aed",
+				"btn-hover": "#6d28d9",
+				"btn-secondary": "#FFD15AFF",
+				"btn-secondary-hover": "#EEC253FF",
+			},
+		},
+	},
+	plugins: [],
+};
